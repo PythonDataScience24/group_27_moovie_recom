60a67872c27180156781e350b117975571bee642
eeewie <126670587+eeewie@users.noreply.github.com>
2024-05-16 12:09:45 +0200

    Added code for liked/recommended movie
    Functionning liked tab
    Placeholding code for recommendation using a base algorithm, will be changed later

--- MovieRecom/recommendation_system.py
+++ MovieRecom/recommendation_system.py
@@ -5,9 +5,12 @@ import requests
 import pickle
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
+from kivy.app import App
+from kivy.uix.label import Label
+
 
 class RecommendationSystem():
-    """Class used to handle user preferances and API calls"""
+    """Class used to handle user preferences and API calls"""
     def __init__(self) -> None:
         self.liked_movies: DataFrame = DataFrame(
             columns=[
@@ -42,28 +45,23 @@ class RecommendationSystem():
         self.df_full = pd.read_csv("finalized_dataset.csv")
 
 
-    def movie_query(self, query:str) -> list[Movie]:
-        """Makes an API call from an user's query and return a list of Movie objects."""
+    def movie_query(self, query: str) -> list[Movie]:
+        """Makes an API call from a user's query and returns a list of Movie objects."""
         if not query:
-            return
+            return []
 
-        # Make API request to OMDB
         api_key = '3ade98ca'  # Replace with your OMDB API key
         url = f'http://www.omdbapi.com/?apikey={api_key}&s={query}&type=movie'
-
         response = requests.get(url)
 
         if response.status_code == 200:
             data = response.json()
             self.loaded_movies.clear()
 
-            
-            # Display search results in MovieList
             if 'Search' in data:
                 for movie in data['Search']:
                     new_movie = Movie()
                     new_movie.from_omdb_dict(movie)
-                    
 
                     url2 = f'http://www.omdbapi.com/?apikey={api_key}&i={new_movie.imdb_id}'
                     response2 = requests.get(url2)
@@ -84,71 +82,71 @@ class RecommendationSystem():
         return self.loaded_movies
 
 
-    def is_movie_liked(self, movie:Movie) -> bool:
-        """Returns whether the provided movie was liked by the user (i.e. is stored in the "liked_movie" DataFrame)."""
+    def is_movie_liked(self, movie: Movie) -> bool:
+        """Returns whether the provided movie was liked by the user."""
         return movie.imdb_id in self.liked_movies['imdb_id'].values
 
-    def is_genre_liked(self, genre:Genre) -> bool:
-        """Returns whether a genre was liked by the user (i.e. is stored in the "liked_genres" Series)."""
+
+    def is_genre_liked(self, genre: Genre) -> bool:
+        """Returns whether a genre was liked by the user."""
         return genre.name in self.liked_genres.values
 
-    def is_director_liked(self, director:Person) -> bool:
-        """Returns whether a director was liked by the user (i.e. is stored in the "liked_directors" Series)."""
-        return director.name in self.liked_directors.values
-    
-    
-    def is_writer_liked(self, writer:Person) -> bool:
-        """Returns whether a writer was liked by the user (i.e. is stored in the "liked_writers" Series)."""
-        return writer.name in self.liked_writers.values
-    
-    
-    def is_actor_liked(self, actor:Person) -> bool:
-        """Returns whether a actor was liked by the user (i.e. is stored in the "liked_actors" Series)."""
-        return actor.name in self.liked_actors.values
-    
 
-    def init_genres_liked(self, genre_list:list[Genre]) -> list[Genre]:
-        """Sets the "liked" field of each genre in the list to true or false depending on if they were liked by the user."""
+    def is_director_liked(self, director: Person) -> bool:
+        """Returns whether a director was liked by the user."""
+        return director.name in self.liked_directors.values
+
+
+    def is_writer_liked(self, writer: Person) -> bool:
+        """Returns whether a writer was liked by the user."""
+        return writer.name in self.liked_writers.values
+
+
+    def is_actor_liked(self, actor: Person) -> bool:
+        """Returns whether an actor was liked by the user."""
+        return actor.name in self.liked_actors.values
+
+
+    def init_genres_liked(self, genre_list: list[Genre]) -> list[Genre]:
+        """Sets the 'liked' field of each genre in the list based on user preference."""
         for i in range(len(genre_list)):
             genre_list[i].liked = self.is_genre_liked(genre_list[i])
         return genre_list
-        
-    
-    def init_directors_liked(self, director_list:list[Person]) -> list[Person]:
-        """Sets the "liked" field of each director in the list to true or false depending on if they were liked by the user."""
+
+
+    def init_directors_liked(self, director_list: list[Person]) -> list[Person]:
+        """Sets the 'liked' field of each director in the list based on user preference."""
         for i in range(len(director_list)):
             director_list[i].liked = self.is_director_liked(director_list[i])
         return director_list
-        
-    
-    
-    def init_writers_liked(self, writer_list:list[Person]) -> list[Person]:
-        """Sets the "liked" field of each writer in the list to true or false depending on if they were liked by the user."""
+
+
+    def init_writers_liked(self, writer_list: list[Person]) -> list[Person]:
+        """Sets the 'liked' field of each writer in the list based on user preference."""
         for i in range(len(writer_list)):
             writer_list[i].liked = self.is_writer_liked(writer_list[i])
         return writer_list
 
-    
-    def init_actors_liked(self, actor_list:list[Person]) -> list[Person]:
-        """Sets the "liked" field of each actor in the list to true or false depending on if they were liked by the user."""
+
+    def init_actors_liked(self, actor_list: list[Person]) -> list[Person]:
+        """Sets the 'liked' field of each actor in the list based on user preference."""
         for i in range(len(actor_list)):
             actor_list[i].liked = self.is_actor_liked(actor_list[i])
         return actor_list
 
 
-    def set_liked_movie(self, movie:Movie, liked:bool):
+    def set_liked_movie(self, movie: Movie, liked: bool):
         """Likes or unlikes a movie."""
         movie.liked = liked
         if liked:
-            df_movie  = DataFrame([movie.to_dict()])
-            self.liked_movies = pd.concat(
-                [self.liked_movies, df_movie],
-                ignore_index=True)
+            df_movie = DataFrame([movie.to_dict()])
+            self.liked_movies = pd.concat([self.liked_movies, df_movie], ignore_index=True)
         else:
             self.liked_movies = self.liked_movies.drop(self.liked_movies[self.liked_movies['imdb_id'] == movie.imdb_id].index)
         print(self.liked_movies)
 
-    def set_liked_genre(self, genre:Genre, liked:bool):
+
+    def set_liked_genre(self, genre: Genre, liked: bool):
         """Likes or unlikes a genre."""
         genre.liked = liked
         if liked:
@@ -156,8 +154,8 @@ class RecommendationSystem():
         else:
             self.liked_genres = self.liked_genres.drop(self.liked_genres[self.liked_genres == genre.name].index)
 
-    
-    def set_liked_director(self, director:Person, liked:bool):
+
+    def set_liked_director(self, director: Person, liked: bool):
         """Likes or unlikes a director."""
         director.liked = liked
         if liked:
@@ -165,7 +163,8 @@ class RecommendationSystem():
         else:
             self.liked_directors = self.liked_directors.drop(self.liked_directors[self.liked_directors == director.name].index)
 
-    def set_liked_writer(self, writer:Person, liked:bool):
+
+    def set_liked_writer(self, writer: Person, liked: bool):
         """Likes or unlikes a writer."""
         writer.liked = liked
         if liked:
@@ -173,38 +172,62 @@ class RecommendationSystem():
         else:
             self.liked_writers = self.liked_writers.drop(self.liked_writers[self.liked_writers == writer.name].index)
 
-        
-    def set_liked_actor(self, actor:Person, liked:bool):
+
+    def set_liked_actor(self, actor: Person, liked: bool):
         """Likes or unlikes an actor."""
         actor.liked = liked
         if liked:
-            self.liked_directors.add(actor.name)
+            self.liked_actors.add(actor.name)
         else:
             self.liked_actors = self.liked_actors.drop(self.liked_actors[self.liked_actors == actor.name].index)
 
 
-    def recommend_movie(self):
-        # get prepared dataset of liked movies
+    def update_liked_movies_ui(self, movie: Movie, add: bool):
+        """Updates the UI to reflect changes in the liked movies list."""
+        print("Attempting to update UI...")
+        try:
+            from movie_list_element import MovieListElement  # Local import to avoid circular dependency
+            app = App.get_running_app()
+            print("App instance:", app)
+            root_widget = app.root
+            print("Root widget:", root_widget)
+            print("Root ids available:", root_widget.ids)
+
+            if 'liked_movies_list' in root_widget.ids:
+                movie_list_widget = root_widget.ids.liked_movies_list
+                print("Movie list widget found:", movie_list_widget)
+
+                if add:
+                    movie_element = MovieListElement(movie, self)
+                    movie_list_widget.add_widget(movie_element)
+                    print(f"Added movie to liked list: {movie.title}")
+                else:
+                    for child in movie_list_widget.children[:]:
+                        if isinstance(child, Label) and child.text == movie.title:
+                            movie_list_widget.remove_widget(child)
+                            print(f"Removed movie from liked list: {movie.title}")
+                            break
+            else:
+                print("liked_movies_list id not found")
+
+        except Exception as e:
+            print(f"Error during UI update: {e}")
+
+
+    def generate_recommendations(self):
+        """Generates movie recommendations based on liked movies."""
         df_liked_movies = self.df_full[self.df_full['tconst'].isin(self.liked_movies["imdb_id"])]
         user_movie_idx = df_liked_movies.index
 
-        # retrieve soup from actors/directors/writes ids + genres
         movies_soup = df_liked_movies['soup']
-        # transform soup to sparse matrix of vectorized values
         count_user_matrix = self.vectorizer.transform(movies_soup)
-        # sum up vectors and normalize it
         count_user_vec = count_user_matrix.sum(axis=0) / count_user_matrix.sum(axis=0).max()
 
-        # calculate similarity
         similarity = cosine_similarity(sparse.csr_matrix(count_user_vec), self.vectorized_dataset)[0]
-        # add info about average rating from all users, to let top movies be higher
         scores = similarity * self.df_full['weightedAverageScales']
 
-        # sort similarities
         similar_scores = list(enumerate(scores))
         similar_scores = sorted(similar_scores, key=lambda x: x[1], reverse=True)
 
-        # retrieve top 10 idx movies to recommend
         recommend_movie_indices = [idx for idx, score in similar_scores if idx not in user_movie_idx][:10]
-        return self.df_full.loc[recommend_movie_indices]
-
+        return self.df_full
