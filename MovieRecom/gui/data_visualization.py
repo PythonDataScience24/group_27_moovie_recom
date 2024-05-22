ca5025032435961a0aeddd90ba301a3b34f5ac4b
Simon Kolly <simon.kolly@students.unibe.ch>
2024-05-22 20:18:18 +0200

    Replace favorite director with favorite actors

--- MovieRecom/gui/data_visualization.py
+++ MovieRecom/gui/data_visualization.py
@@ -19,7 +19,7 @@ def create_liked_visualizations(liked_movie_list: pd.DataFrame) -> plt.Figure:
     """Create visualizations for liked movies:
         - pie plot of genre
         - box plot of runtime
-        - bar chart of director
+        - bar chart of actors
     """
 
     # create genre pie chart data
@@ -32,29 +32,29 @@ def create_liked_visualizations(liked_movie_list: pd.DataFrame) -> plt.Figure:
                 ignore_index=True)
     split_genre_df = split_genre_df.groupby(['genre']).count()
 
-    # create director bar chart data
-    director_df = liked_movie_list[['imdb_id', 'director']]
-    split_director_df = pd.DataFrame()
-    for index, entry in director_df.iterrows():
-        for director in entry['director']:
-            split_director_df = pd.concat(
-                [pd.DataFrame(data=[[entry['imdb_id'], str(director)]], columns=director_df.columns), split_director_df], 
+    # create actors bar chart data
+    actors_df = liked_movie_list[['imdb_id', 'actors']]
+    split_actors_df = pd.DataFrame()
+    for index, entry in actors_df.iterrows():
+        for actor in entry['actors']:
+            split_actors_df = pd.concat(
+                [pd.DataFrame(data=[[entry['imdb_id'], str(actor)]], columns=actors_df.columns), split_actors_df], 
                 ignore_index=True)
-    split_director_df = split_director_df.groupby(['director']).count()
+    split_actors_df = split_actors_df.groupby(['actors']).count()
 
     fig, ax = plt.subplots(3, 1) # size not fixed yet
     plt.close()
     fig.tight_layout(h_pad=1.0)
 
-    ax[0].set_title("favourite genres")
+    ax[0].set_title("favorite genres")
     ax[0].pie(x=split_genre_df['imdb_id'], labels=split_genre_df.index)
 
     ax[1].set_title("movie runtime")
     ax[1].violinplot(dataset=liked_movie_list['runtime'].astype(int), vert=False, showmeans=True)
     #ax[1].boxplot(x=liked_movie_list['runtime'].astype(int), vert=False, notch=True)
 
-    ax[2].set_title("directors")
-    ax[2].bar(x=split_director_df.index, height=split_director_df['imdb_id'])
+    ax[2].set_title("favorite actors")
+    ax[2].bar(x=split_actors_df.index, height=split_actors_df['imdb_id'])
 
     print("Visualization created!")
 
